b4845f3740b97861b74940e3c40e5e9b58cf7b79
benson-optimiste1 <chatelainevelybenson@gmail.com>
2023-11-08 09:52:29 -0500

    c12-blog-app

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+  // useEffect is being called on different lifecycle
+
+  const handleFetchData = () => {
+    console.log('fetching data')
+    
+    fetch('https://localhost:8080')
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err))
+
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hey Class</Text>
       <StatusBar style="auto" />
+      <Button title='get some data' onPress={(handleFetchData) => console.log('test')}/>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create ({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     alignItems: 'center',
     justifyContent: 'center',
-  },
-});
+  }
+})
