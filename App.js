df9e5a60f9ee4038622bff6f74be7569b6390bdb
benson-optimiste1 <chatelainevelybenson@gmail.com>
2023-11-08 13:24:00 -0500

    PostCard added

--- App.js
+++ App.js
@@ -1,33 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
-
+import { StyleSheet, Text, View, Button, Image, ScrollView, SafeAreaView } from 'react-native';
+import { useEffect, useState } from 'react';
+import PostCard from './components/PostCard';
 export default function App() {
 
-  // useEffect is being called on different lifecycle
+  const [blogPosts, setBlogPosts] = useState([])
 
-  const handleFetchData = () => {
-    console.log('fetching data')
-    
-    fetch('https://localhost:8080')
+  // useEffect is being called on different lifecycle of the component
+  // 1. when component Mounts
+  // 2. when component UnMonths
+
+  // How to get a component to re-render
+  // 1.change state variable
+  // 2. change props
+
+  useEffect(() => {
+    fetch('http://192.168.10.118:8080')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => setBlogPosts(data))
     .catch(err => console.error(err))
 
-  }
-  return (
-    <View style={styles.container}>
-      <Text>Hey Class</Text>
-      <StatusBar style="auto" />
-      <Button title='get some data' onPress={(handleFetchData) => console.log('test')}/>
-    </View>
-  );
-}
+  }, [])
 
-const styles = StyleSheet.create ({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: 'center',
-    justifyContent: 'center',
+
+  return (
+    
+      <SafeAreaView style={styles.container}>
+        <StatusBar style="auto" />
+        
+        <View>
+          <ScrollView>
+            
+            {blogPosts.map((singlePost, index) => {
+              //single post in an object within an array
+              console.log(singlePost)
+              return (
+                <PostCard singlePost={singlePost} index={index} key={singlePost._id}/>
+              )
+            })}
+          </ScrollView>
+        </View>
+      </SafeAreaView>
+    )
   }
-})
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    }
+  })
